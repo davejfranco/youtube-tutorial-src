297a05401274dfeb1a87b76ce0bae0ba0eab1a90
Dave <davefranco1987@gmail.com>
2023-02-15 19:13:57 +0100

    src moved to a specific directory

---
+++ tutoriales/terraform/eks/terraform.tf
@@ -0,0 +1,12 @@
+terraform {
+  # Minimum required version
+  required_version = ">= 1.3.7"
+
+  required_providers {
+    aws = {
+      source = "registry.terraform.io/hashicorp/aws"
+    }
+  }
+}
+
+
---
+++ tutoriales/terraform/github-action/datasource.tf
@@ -0,0 +1,3 @@
+data "aws_iam_user" "me" {
+  user_name = "daveops"
+}
