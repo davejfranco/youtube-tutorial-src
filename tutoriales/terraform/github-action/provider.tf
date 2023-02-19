49ebdddff81b87f6de39c8086f91c3744d199b4d
Dave <davefranco1987@gmail.com>
2023-02-19 17:51:51 +0100

    no longer needed files

--- tutoriales/terraform/github-action/provider.tf
+++
@@ -1,8 +0,0 @@
-provider "aws" {
-  region                   = "us-east-1"
-  shared_config_files      = ["~/.aws/config"]
-  shared_credentials_files = ["~/.aws/credentials"]
-  profile                  = "personal"
-
-}
-
