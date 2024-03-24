9541faab144d7ad43dca8e2defaf239d29a953e5
Dave Franco <davefranco1987@gmail.com>
2024-03-24 14:27:39 +0100

    ack deployment
    - ack script to install and uninstall ack controllers including permissions
    - fix node networking on eksctl config file
    - infra yaml to deploy securitygroup and ec2 instance

--- tutoriales/kubernetes/ack/ack.sh
+++ tutoriales/kubernetes/ack/ack.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
 
-EKS_CLUSTER_NAME=$1
-AWS_REGION=$2
-
-if [ -z "$EKS_CLUSTER_NAME" ]; then
-    echo "Usage: $0 <EKS_CLUSTER_NAME> <AWS_REGION>"
-    exit 1
-fi
-
-export AWS_REGION=$AWS_REGION
+EKS_CLUSTER_NAME=$2
+AWS_REGION=$3
 
 SERVICES="ec2 s3"
 ACK_SYSTEM_NAMESPACE="ack-system"
-ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
 
-create_oidc() {
+function add_oidc() {
+    if [ -z "$EKS_CLUSTER_NAME" ]; then
+      help
+      exit 1
+    fi
+
     echo "Creating OIDC provider for EKS cluster $EKS_CLUSTER_NAME in region $AWS_REGION..."
     eksctl utils associate-iam-oidc-provider --cluster $EKS_CLUSTER_NAME --region $AWS_REGION --approve
 }
 
-iam_role() {  
-  echo "Creating IAM roles for ACK controllers..."
-  OIDC_PROVIDER=$(aws eks describe-cluster --name $EKS_CLUSTER_NAME --query "cluster.identity.oidc.issuer" --output text | sed -e "s/^https:\/\///")
+function add_iam_role() {
+  if [ -z "$EKS_CLUSTER_NAME" ]; then
+    help
+    exit 1
+  fi
 
+  echo "Creating IAM roles for ACK controllers..."
+  OIDC_PROVIDER=$(aws eks describe-cluster --name $EKS_CLUSTER_NAME --region $AWS_REGION --query "cluster.identity.oidc.issuer" --output text | sed -e "s/^https:\/\///")
+
+  ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
   for service in $SERVICES; do
     ACK_SERVICE_ACCOUNT_NAME="ack-$service-controller"
     
@@ -33,7 +36,7 @@ iam_role() {
     {
       "Effect": "Allow",
       "Principal": {
-        "Federated": "arn:aws:iam::${AWS_ACCOUNT_ID}:oidc-provider/${OIDC_PROVIDER}"
+        "Federated": "arn:aws:iam::${ACCOUNT_ID}:oidc-provider/${OIDC_PROVIDER}"
       },
       "Action": "sts:AssumeRoleWithWebIdentity",
       "Condition": {
@@ -80,18 +83,19 @@ echo "${TRUST_RELATIONSHIP}" > trust.json
 
 }
 
-install_controllers() {
+function install_controllers() {
   echo "login into public ECR"
 
-  #aws ecr-public get-login-password --region us-east-1 | helm registry login --username AWS --password-stdin public.ecr.aws
-   
+  aws ecr-public get-login-password --region us-east-1 | helm registry login --username AWS --password-stdin public.ecr.aws
+  ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
   for service in $SERVICES; do
     
     RELEASE_VERSION=$(curl -sL https://api.github.com/repos/aws-controllers-k8s/${service}-controller/releases/latest | jq -r '.tag_name | ltrimstr("v")')
     ACK_CONTROLLER_IAM_ROLE_ARN="arn:aws:iam::$ACCOUNT_ID:role/ack-$service-controller-role" 
     #echo $ACK_CONTROLLER_IAM_ROLE
-    helm template -n $ACK_SYSTEM_NAMESPACE ack-$service-controller \
+    helm upgrade --install -n $ACK_SYSTEM_NAMESPACE ack-$service-controller \
       oci://public.ecr.aws/aws-controllers-k8s/$service-chart \
+      --create-namespace \
       --version=$RELEASE_VERSION \
       --set="serviceAccount.annotations.eks\.amazonaws\.com/role-arn=$ACK_CONTROLLER_IAM_ROLE_ARN" \
       --set=aws.region=$AWS_REGION
@@ -99,5 +103,63 @@ install_controllers() {
 
 }
 
-install_controllers
+function delete_controllers() {
+  for service in $SERVICES; do
+    helm uninstall -n $ACK_SYSTEM_NAMESPACE ack-$service-controller
+  done
+}
 
+function delete_iam_role() {
+  for service in $SERVICES; do
+    ACK_CONTROLLER_IAM_ROLE="ack-$service-controller-role"
+    #get attached policies
+    ATTACHED_POLICIES=$(aws iam list-attached-role-policies --role-name $ACK_CONTROLLER_IAM_ROLE --query 'AttachedPolicies[].PolicyArn' --output text)
+    INLINE_POLICIES=$(aws iam list-role-policies --role-name $ACK_CONTROLLER_IAM_ROLE --query 'PolicyNames' --output text)
+    #detach AttachedPolicies
+    for policy in $ATTACHED_POLICIES; do
+      aws iam detach-role-policy --role-name $ACK_CONTROLLER_IAM_ROLE --policy-arn $policy
+    done
+    #delete inline policies
+    for policy in $INLINE_POLICIES; do
+      aws iam delete-role-policy --role-name $ACK_CONTROLLER_IAM_ROLE --policy-name $policy
+    done
+    aws iam delete-role --role-name $ACK_CONTROLLER_IAM_ROLE
+  done
+}
+
+function help() {
+  echo "Usage: $0  <install|uninstall|add-oidc|iam-role|install-controllers|delete-controllers|delete-iam-role|help> <EKS_CLUSTER_NAME> <AWS_REGION>" 
+}
+case $1 in
+  install)
+    add_oidc
+    add_iam_role
+    install_controllers
+    ;;
+  uninstall)
+    delete_controllers
+    delete_iam_role
+    ;;
+  add-oidc)
+    add_oidc
+    ;;
+  create-iam-role)
+    add_iam_role
+    ;;
+  install-controllers)
+    install_controllers
+    ;;
+  delete-controllers)
+    delete_controllers
+    ;;
+  delete-iam-role)
+    delete_iam_role
+    ;;
+  help)
+    help
+    ;;
+  *)
+    help
+    exit 1
+    ;;
+esac
